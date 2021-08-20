ff78384de5f204bdab456bb03ca280a62e41ff0c
Vong Nyuk Soon <vongnyuksoon2000@gmail.com>
2021-08-20 18:46:45 +0800

    chore: Add qrcode scanner dependency

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:penger/helpers/theme/custom_font.dart';
+import 'package:penger/ui/qr/qr_scanner_view.dart';
+import 'package:penger/ui/widgets/button/custom_button.dart';
 import 'package:penger/ui/widgets/layout/sliver_appbar.dart';
 import 'package:penger/ui/widgets/layout/sliver_body.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,6 +19,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -20,45 +42,65 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          CustomSliverAppBar(
+            title: Text(
+              "Home",
+              style: PengoStyle.navigationTitle(context),
+            ),
+          ),
+          CustomSliverBody(
+            content: <Widget>[
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: <Widget>[
+                    CupertinoSearchTextField(
+                      onChanged: (String value) {
+                        debugPrint('The text has changed to: $value');
+                      },
+                      onSubmitted: (String value) {
+                        debugPrint('Submitted text: $value');
+                      },
+                    ),
+                    CustomButton(
+                      text: Text("Scan"),
+                      onPressed: () {
+                        Navigator.of(context).push(CupertinoPageRoute(
+                          builder: (context) => QRScannerPage(),
+                        ));
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
   }
 
   @override
-  Widget build(BuildContext context) {
-    final TextTheme textTheme = Theme.of(context).textTheme;
-    return CustomScrollView(
-      slivers: <Widget>[
-        CustomSliverAppBar(
-          title: Text(
-            "Home",
-            style: PengoStyle.navigationTitle(context),
-          ),
-        ),
-        CustomSliverBody(
-          content: <Widget>[
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: <Widget>[
-                  CupertinoSearchTextField(
-                    onChanged: (String value) {
-                      debugPrint('The text has changed to: $value');
-                    },
-                    onSubmitted: (String value) {
-                      debugPrint('Submitted text: $value');
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ],
-    );
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
